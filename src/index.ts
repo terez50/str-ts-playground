7adaa38957c6e14b78ad43bbe5f09161503511ee
cherryApp <j.cserko@gmail.com>
2021-01-19 13:43:46 +0100

    oop01

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import './style.scss';
 
-const user = {
-    name: 'Juli',
-    email: 'juli@legjobbangoltanar.hu',
-};
+class User {
+    name: string = '';
+    email: string = '';
+    age: number = 18;
+
+    constructor(name: string) { 
+        this.name = name;
+    }
+
+    hello(): string {
+        return `Szia, a nevem ${this.name}`;
+    }
+}
+
+const user1 = new User('Pisti');
+const user2 = new User('Sanyi');
+const user3 = new User('Enikő');
+
+console.log(user3.age, user3.name);
